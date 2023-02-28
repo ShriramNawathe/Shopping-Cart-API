86b389cef1ec944d1ac183706db236fe868038d5
Shriram26101999 <108632713+Shriram26101999@users.noreply.github.com>
2023-02-28 18:20:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-
+// A function named `jsonFetch` which returns a Promise
 let jsonFetch = (e) => {
     return new Promise((resolve, reject) => {
-      
+         // the fetch API to retrieve data from the specified file
       fetch("list.json")
         .then((r) => {
-        
+  // If the API response is successful, resolve the Promise with the JSON data
           if (r.ok) {
             resolve(r.json());
           } else {
-      
+   // Otherwise, reject the Promise with an error message
             reject(new Error("Failed to retrieve data from API"));
           }
         })
         .catch((e) => {
-      
+    // Catch any network or fetch-related errors and reject the Promise with the error
           reject(e);
         });
     });
   }; 
+// Call the `fetchData` function
   jsonFetch()
     .then((eleInfo) => {
+ // If the Promise returned by `fetchData` is resolved, the code inside this block will be executed
+    // Use the data retrieved from the API      
       let eleC = document.querySelector(".eleC"); 
-      
+  // Iterate over the products in the data and create a product element for each one     
       eleInfo.products.forEach((ele) => {   
           let eleD = document.createElement("span");
           eleD.classList.add("elementList");
+     // Create the HTML for the product element using template literals        
           eleD.innerHTML = `
           <div>
             <img src="${ele.thumbnail}" alt="${ele.title}" class="eleImage" id="eleImage">
@@ -48,7 +52,7 @@ let jsonFetch = (e) => {
                 
                         
                 
-               
+// Append the product element to the product container               
                
                 
            `;
@@ -56,6 +60,7 @@ let jsonFetch = (e) => {
        });
     })
     .catch(() => {
-      
+  // If the Promise returned by `fetchData` is rejected, the code inside this block will be executed
+    // Handle any errors that occurred during the API call    
       console.error();
     });
